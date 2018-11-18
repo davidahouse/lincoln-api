9433708a687bdca435c5f718f71cf8602a8c1db7
David House <david.house@kp.org>
2018-11-18 18:33:43 -0500

    documentation and lint cleanup

--- README.md
+++ README.md
@@ -12,3 +12,33 @@ $ npm install @davidahouse/lincoln-api
 ```
 
 ## Usage
+
+Configuring the API is easy, just specify the vault you want to interact with by name:
+
+```
+var LincolnVault = require('@davidahouse/lincoln-api')
+
+var vault = new LincolnVault("yourvaultname")
+```
+
+You can get the path to any of the Lincoln containers by using the `pathFor` method:
+
+```
+var path = vault.pathFor("Today")
+```
+
+The container names are as follows:
+
+| Container | Description |
+|-----------|-------------|
+| Today     | The journal folder that points to today's date |
+| Journal/YYYY-MM-DD | The journal folder for a specific date |
+| Glance    | The glance container |
+| Frequent  | The frequent container |
+| Content/optional_folder_path | A content container. The folder path represents the folders under the main content root, but is optional |
+
+You can create a new markdown document using the `newDocument` method:
+
+```
+vault.newDocument("Glance", "My Document", "## The markdown contents go here")
+```
--- index.js
+++ index.js
@@ -2,44 +2,52 @@ const fs = require('fs')
 const homedir = require('os').homedir()
 const moment = require('moment')
 
+/**
+* This class implements an interface to a Lincoln vault, which is the
+* top level data structure used in the Lincoln app file system.
+*/
 class LincolnVault {
+  /**
+  * Constructor for the LincolnVault class
+  * @param {string} vault The name of the vault
+  */
   constructor(vault) {
-    let self = this
     this.vault = vault
-    var contents = fs.readFileSync(homedir + "/Library/Application\ Support/com.repairward.lincoln/vaults/" + vault + ".vaultConfig")
+    const vaultConfigPath = homedir + '/Library/Application Support/com.repairward.lincoln/vaults/'
+    const contents = fs.readFileSync( vaultConfigPath + vault + '.vaultConfig')
     this.config = JSON.parse(contents)
 
     this.contents = function(container) {
-
       fs.readdir(this.pathFor(container), function(err, items) {
-        for (var i=0; i<items.length; i++) {
-          console.log(items[i])
-        }
+        return items
       })
     }
 
     this.pathFor = function(container) {
-
-      if (container.toLowerCase() == "today") {
-        var today = moment(new Date())
-        return this.config.rootFolder + "/__journal/" + today.format("MMM_YYYY") + "/" + today.format("MMM_DD_YYYY") + "/"
-      } else if (container.toLowerCase() == "glance") {
-        return this.config.rootFolder + "/__glance/"
-      } else if (container.toLowerCase() == "frequent") {
-        return this.config.rootFolder + "/__frequent/"
-      } else if (container.toLowerCase().startsWith("content")) {
-        return this.config.rootFolder + "/__" + container.toLowerCase() + "/"
-      } else if (container.toLowerCase().startsWith("journal")) {
-        var parts = container.split('/')
-        var journal = moment(parts[1])
-        return this.config.rootFolder + "/__journal/" + journal.format("MMM_YYYY") + "/" + journal.format("MMM_DD_YYYY") + "/"
+      if (container.toLowerCase() == 'today') {
+        const today = moment(new Date())
+        const monthFolder = today.format('MMM_YYYY')
+        const dayFolder = today.format('MMM_DD_YYYY')
+        return this.config.rootFolder + '/__journal/' + monthFolder + '/' + dayFolder + '/'
+      } else if (container.toLowerCase() == 'glance') {
+        return this.config.rootFolder + '/__glance/'
+      } else if (container.toLowerCase() == 'frequent') {
+        return this.config.rootFolder + '/__frequent/'
+      } else if (container.toLowerCase().startsWith('content')) {
+        return this.config.rootFolder + '/__' + container.toLowerCase() + '/'
+      } else if (container.toLowerCase().startsWith('journal')) {
+        const parts = container.split('/')
+        const journal = moment(parts[1])
+        const monthFolder = journal.format('MMM_YYYY')
+        const dayFolder = journal.format('MMM_DD_YYYY')
+        return this.config.rootFolder + '/__journal/' + monthFolder + '/' + dayFolder + '/'
       } else {
         return this.config.rootFolder
       }
     }
 
     this.newDocument = function(container, title, contents) {
-      var path = this.pathFor(container) + "/"+ title + ".md"
+      const path = this.pathFor(container) + '/'+ title + '.md'
       fs.writeFileSync(path, contents)
     }
   }
